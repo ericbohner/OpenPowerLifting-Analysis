b68128db27704a7ad0134f8f5690d59b41d0b797
Eric Bohner <eric.n.bohner@gmail.com>
2023-08-09 12:02:24 -0600

    Initial Commit

---
+++ database_setup.sql
@@ -0,0 +1,66 @@
+-- Create the 'ipf' schema if it doesn't exist
+CREATE DATABASE IF NOT EXISTS ipf;
+
+-- Use the 'ipf' schema
+USE ipf;
+
+-- For those of us who run scripts more than once
+DROP TABLE IF EXISTS ipf;
+
+-- Create the 'ipf' table with appropriate columns and data types
+CREATE TABLE ipf (
+    AthleteName VARCHAR(100),
+    Sex CHAR(2),
+    IPFEvent VARCHAR(3),
+    Equipment VARCHAR(10),
+    Age INT,
+    AgeClass VARCHAR(6),
+    BirthYearClass VARCHAR(6),
+    Division VARCHAR(255),
+    BodyweightKg DECIMAL(10, 2),
+    WeightClassKg VARCHAR(10),
+    Squat1Kg DECIMAL(10, 2),
+    Squat2Kg DECIMAL(10, 2),
+    Squat3Kg DECIMAL(10, 2),
+    Squat4Kg DECIMAL(10, 2),
+    Best3SquatKg DECIMAL(10, 2),
+    Bench1Kg DECIMAL(10, 2),
+    Bench2Kg DECIMAL(10, 2),
+    Bench3Kg DECIMAL(10, 2),
+    Bench4Kg DECIMAL(10, 2),
+    Best3BenchKg DECIMAL(10, 2),
+    Deadlift1Kg DECIMAL(10, 2),
+    Deadlift2Kg DECIMAL(10, 2),
+    Deadlift3Kg DECIMAL(10, 2),
+    Deadlift4Kg DECIMAL(10, 2),
+    Best3DeadliftKg DECIMAL(10, 2),
+    TotalKg DECIMAL(10, 2),
+    Place VARCHAR(5),
+    Dots DECIMAL(10, 2),
+    Wilks DECIMAL(10, 2),
+    Glossbrenner DECIMAL(10, 2),
+    Goodlift DECIMAL(10, 2),
+    Tested VARCHAR(10),
+    Country VARCHAR(255),
+    CountryState VARCHAR(255),
+    Federation VARCHAR(255),
+    ParentFederation VARCHAR(255),
+    EventDate DATE,
+    MeetCountry VARCHAR(255),
+    MeetState VARCHAR(255),
+    MeetTown VARCHAR(255),
+    MeetName VARCHAR(255)
+);
+
+-- Find out where to save the csv in order to load it into my table
+SHOW VARIABLES LIKE 'secure_file_priv';
+
+-- Load data from the CSV file 'openipf-2023-08-05-ae3dc469'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\openpl.csv'
+INTO TABLE ipf
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Let's look at what we just created
+DESCRIBE ipf;
---
+++ eda.sql
@@ -0,0 +1,29 @@
+USE ipf;
+
+
+-- I want to know how many weight classes there are
+SELECT 
+    COUNT(DISTINCT WeightClassKg) as unique_weightclasses
+FROM ipf;
+
+-- And how many entries are in each weight class
+SELECT 
+    WeightClassKg
+    , COUNT(WeightClassKg) as entries_by_weightclass
+FROM ipf
+GROUP BY WeightClassKg
+ORDER BY entries_by_weightclass DESC;
+
+/* Many of the weight classes only have a single entry.  A weight class of 103.3, for instance,
+doesn't make a lot of sense, so is probably user input error.  It is likely the weight of the
+athlete, and not the weight class that the athlete was competing in. Alternatively, it might be
+a conversion between lbs and kg.  For instance, the 92.9kg weight class might be the -205lbs weight
+class. All of these need to be dealt with to be able to draw meaningful conclusions from them.*/
+
+SELECT 
+    WeightClassKg
+    , COUNT(WeightClassKg) as entries
+FROM ipf
+GROUP BY WeightClassKg
+ORDER BY WeightClassKg DESC;
+
