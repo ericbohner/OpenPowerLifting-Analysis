f70734a5408299da66a3b4456f32285d25957be5
Eric Bohner <eric.n.bohner@gmail.com>
2023-08-14 10:28:42 -0600

    basic sql queries added to eda.sql

--- database_setup.sql
+++ database_setup.sql
@@ -1,24 +1,26 @@
--- Create the 'ipf' schema if it doesn't exist.
-CREATE DATABASE IF NOT EXISTS ipf;
+-- Create the 'openpl' schema if it doesn't exist.
+CREATE DATABASE IF NOT EXISTS openpl;
 
--- Use the 'ipf' schema
-USE ipf;
+-- Use the 'openpl' schema
+USE openpl;
 
 -- For those of us who run scripts more than once
-DROP TABLE IF EXISTS ipf;
+DROP TABLE IF EXISTS openpl;
 
 -- Create the 'ipf' table with appropriate columns and data types
-CREATE TABLE ipf (
+CREATE TABLE openpl (
+    MeetID INT,
+    AthleteID INT,
     AthleteName VARCHAR(100),
     Sex CHAR(2),
-    IPFEvent VARCHAR(3),
+    EventClass VARCHAR(3),
     Equipment VARCHAR(10),
-    Age INT,
+    Age DECIMAL (10,2),
     AgeClass VARCHAR(6),
     BirthYearClass VARCHAR(6),
     Division VARCHAR(255),
     BodyweightKg DECIMAL(10, 2),
-    WeightClassKg VARCHAR(10),
+    WeightClassKg DECIMAL(10, 2),
     Squat1Kg DECIMAL(10, 2),
     Squat2Kg DECIMAL(10, 2),
     Squat3Kg DECIMAL(10, 2),
@@ -55,12 +57,118 @@ CREATE TABLE ipf (
 -- Find out where to save the csv in order to load it into my table
 SHOW VARIABLES LIKE 'secure_file_priv';
 
--- Load data from the CSV file 'openipf-2023-08-05-ae3dc469'
+-- Load data from the CSV file 'openpl.csv'
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\openpl.csv'
-INTO TABLE ipf
+INTO TABLE openpl
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 -- Let's look at what we just created
-DESCRIBE ipf;
+DESCRIBE openpl;
+
+-- quick look at the data
+SELECT *
+FROM openpl
+LIMIT 10;
+
+-- We expect 2932694 rows.  Check to make sure.
+SELECT COUNT(AthleteID)
+FROM openpl;
+
+-- I want to see something simple to see if numbers seem approximately correct
+SELECT 
+	Equipment,
+    COUNT(AthleteID)
+FROM openpl
+GROUP BY Equipment;
+
+-- Let's make sure these numbers add up to the total in our database
+WITH equip_counts AS (
+	SELECT 
+		Equipment,
+		COUNT(AthleteID) AS num_athletes
+	FROM openpl
+	GROUP BY Equipment
+)
+SELECT SUM(num_athletes) AS total_athletes
+FROM equip_counts;
+
+
+/* 
+I'll now do some data normalization.  Since we are working with a large number of columns, many parts of this data
+can be split into different tables to reduce data redundancies and make data maintenance much easier.
+*/
+
+CREATE TABLE Athletes (
+    AthleteID INT PRIMARY KEY,
+    AthleteName VARCHAR(100),
+    Sex CHAR(2),
+    -- Other attributes related to the athlete
+);
+
+
+CREATE TABLE Events (
+    EventID INT PRIMARY KEY,
+    IPFEvent VARCHAR(3),
+    Equipment VARCHAR(10),
+    -- Other attributes related to the event
+);
+
+
+CREATE TABLE WeightClasses (
+    WeightClassID INT PRIMARY KEY,
+    WeightClassKg DECIMAL(10, 2),
+    -- Other attributes related to weight classes
+);
+
+
+CREATE TABLE AthleteWeightClasses (
+    AthleteID INT,
+    WeightClassID INT,
+    PRIMARY KEY (AthleteID, WeightClassID),
+    FOREIGN KEY (AthleteID) REFERENCES Athletes(AthleteID),
+    FOREIGN KEY (WeightClassID) REFERENCES WeightClasses(WeightClassID)
+);
+
+
+CREATE TABLE Lifts (
+    LiftID INT AUTO_INCREMENT PRIMARY KEY,
+    AthleteID INT,
+    EventID INT,
+    Squat1Kg DECIMAL(10, 2),
+    Squat2Kg DECIMAL(10, 2),
+    Squat3Kg DECIMAL(10, 2),
+    Bench1Kg DECIMAL(10, 2),
+    Bench2Kg DECIMAL(10, 2),
+    Bench3Kg DECIMAL(10, 2),
+    Deadlift1Kg DECIMAL(10, 2),
+    Deadlift2Kg DECIMAL(10, 2),
+    Deadlift3Kg DECIMAL(10, 2),
+    -- Other lift-specific attributes
+    FOREIGN KEY (AthleteID) REFERENCES Athletes(AthleteID),
+    FOREIGN KEY (EventID) REFERENCES Events(EventID)
+);
+
+
+CREATE TABLE MeetInfo (
+    MeetID INT PRIMARY KEY,
+    MeetCountry VARCHAR(255),
+    MeetState VARCHAR(255),
+    MeetTown VARCHAR(255),
+    MeetName VARCHAR(255),
+    EventDate DATE,
+    -- Other attributes related to meet
+);
+
+
+CREATE TABLE AthleteMeet (
+    AthleteID INT,
+    MeetID INT,
+    PRIMARY KEY (AthleteID, MeetID),
+    FOREIGN KEY (AthleteID) REFERENCES Athletes(AthleteID),
+    FOREIGN KEY (MeetID) REFERENCES MeetInfo(MeetID)
+);
+
+
+
--- eda.sql
+++ eda.sql
@@ -1,29 +1,82 @@
-USE ipf;
+USE openpl;
 
+-- sneak a peak
+SELECT *
+FROM openpl
+LIMIT 10;
 
--- I want to know how many weight classes there are in total.
+-- get an idea of the timeframe that is included in the database and how many events have been recorded
 SELECT 
-    COUNT(DISTINCT WeightClassKg) as unique_weightclasses
-FROM ipf;
+	MIN(EventDate) as earliest_event,
+    MAX(EventDate) as latest_event,
+    COUNT(DISTINCT(MeetID)) as num_events,
+    COUNT(DISTINCT(AthleteID)) as num_athletes
+FROM openpl;
 
--- And how many entries are in each weight class
+-- identify the popularity of different equipment types (raw, single ply, multi, etc.)
+SELECT
+	Equipment,
+    COUNT(DISTINCT(MeetID)) as num_meets, -- only count unique events
+    COUNT(AthleteID) as num_athletes -- note, athletes can compete in multiple events and equip types
+FROM openpl
+GROUP BY Equipment
+ORDER BY num_athletes DESC;
+
+/* 
+It looks like the most popular equipment types are singe-ply and raw.  I suspect, however, that this doesn't tell the whole story
+since different year ranges will have differences in popularity for a given equipment type.  For instance, I think that raw has
+become more popular recently (since, say, 2010?).  Let's check.
+*/
+
+SELECT
+	Equipment,
+    COUNT(DISTINCT(MeetID)) as num_meets, -- only count unique events
+    COUNT(AthleteID) as num_athletes -- athletes can compete in multiple events
+FROM openpl
+WHERE YEAR(EventDate) >= 2010 
+GROUP BY Equipment
+ORDER BY num_athletes DESC;
+
+/*
+Note that there are now far more Raw events and competitors than single-ply. It also seems as though MOST of the raw events have
+taken place after 2010, with 23,521 out of a total 25,640 raw events.  Since it covers years from 1964 - 2023, we can see that raw
+events have almost entirely overtaken powerlifting in recent years.
+*/
+
+
+-- What is the current S/B/D and total record for each equipment type and weight class?
+
+SET group_concat_max_len = 1000000;
+
+DROP TABLE IF EXISTS CurrentRecordHolders;
+CREATE TABLE CurrentRecordHolders AS
 SELECT 
-    WeightClassKg
-    , COUNT(WeightClassKg) as entries_by_weightclass
-FROM ipf
-GROUP BY WeightClassKg
-ORDER BY entries_by_weightclass DESC;
+	Equipment
+    , Sex 
+    , WeightClassKg
+    , MAX(Best3SquatKg) AS squat_record
+    , SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT AthleteName ORDER BY Best3SquatKg DESC), ',', 1) AS squat_record_holder
+    , MAX(Best3BenchKg) AS bench_record
+    , SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT AthleteName ORDER BY Best3BenchKg DESC), ',', 1) AS bench_record_holder
+    , MAX(Best3DeadliftKg) AS deadlift_record
+    , SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT AthleteName ORDER BY Best3DeadliftKg DESC), ',', 1) AS deadlift_record_holder
+    , MAX(TotalKg) AS total_record
+    , SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT AthleteName ORDER BY TotalKg DESC), ',', 1) AS total_record_holder
+FROM openpl
+WHERE Place != 'DQ'
+GROUP BY Equipment, Sex, WeightClassKg
+ORDER BY Equipment, Sex, WeightClassKg;
 
-/* Many of the weight classes only have a single entry.  A weight class of 103.3, for instance,
-doesn't make a lot of sense, so is probably user input error.  It is likely the weight of the
-athlete, and not the weight class that the athlete was competing in. Alternatively, it might be
-a conversion between lbs and kg.  For instance, the 92.9kg weight class might be the -205lbs weight
-class. All of these need to be dealt with to be able to draw meaningful conclusions from them.*/
+SELECT *
+FROM CurrentRecordHolders;
 
-SELECT 
-    WeightClassKg
-    , COUNT(WeightClassKg) as entries
-FROM ipf
-GROUP BY WeightClassKg
-ORDER BY WeightClassKg DESC;
 
+
+
+-- find out how many non-binary competitors there have been, their biggest totals, and avg totals compared to M/F competitors
+SELECT
+	Sex,
+    avg(TotalKg) as avg_total,
+    max(TotalKg) as max_total
+FROM openpl
+group by Sex;
